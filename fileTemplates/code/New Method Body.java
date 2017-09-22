2c7edd26ad1a4d3d8f5586108c0a4010c906d7c4
Chris Kent <chris@opengamma.com>
2017-09-22 11:19:01 +0100

    IU-173.2290.1 <chris@Chris-Kents-Mac-Pro.local Create markdown.xml, Inconsolata.icls, Consolas.icls, Default copy.xml, Django.xml, Default.xml, find.xml, CJ.icls, Clojure.xml, Web Services.xml, path.macros.xml, Default.xml, JavaScript.xml, Monaco.icls, debugger.renderers.xml, Kotlin.xml, plain.xml, asciidoc.xml, surround.xml, Courier.icls, keymap.xml, editor.xml, debugger.xml, Andale Mono.icls, baseRefactoring.xml, AMD JavaScript File.js, ide.general.xml, user.xml, git.xml, Zen CSS.xml, ui.lnf.xml, filetypes.xml, javaeeExternalResources.xml, plugin_ui.xml, html_xml.xml, notifications.xml, databaseDrivers.xml, AnnotationType.java, SQL.xml, jdk.table.xml, CJ.xml, Mac OS X 10_5_ copy.xml, web-browsers.xml, macros.xml, toStringTemplates.xml, mono.icls, vera.icls, Implemented Method Body.java, Dockerfile.xml, Clojure.xml, Darcula small.icls, IntelliLang.xml, Rust.xml, print.xml, TextMate RSpec.xml, project.default.xml, projectStructureProblems.xml, diff.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, TestNG Test Method.java, JUnit4 Test Method.java, file.template.settings.xml, File Header.java, gradle.run.settings.xml, code.style.schemes.xml, New Kotlin Function Body.kt, Zen HTML.xml, github_settings.xml, mavenVersion.xml, JavaScript File.js, Lucida Console 1_1.icls, flask.xml, CJ.xml, testgroup.xml, TestNG Test Class.java, duplocatorSettings.xml, debugger.frameview.xml, encoding.xml, editor.codeinsight.xml, OpenGamma.xml, applicationLibraries.xml, Lucida Console.icls, Test Class.java, JavaFXApplication.java, getterTemplates.xml, other.xml, TextMate Cucumber.xml, xpath.xml, GroovyClass.groovy, scratch_config.xml, Deployment.xml, colors.scheme.xml, External Tools.xml, templates.xml, Menlo.icls, consoleFolding.xml, customization.xml, Ruby_Rails.xml, output.xml, Dynamic Method Body.java, Class.java, GroovyScript.groovy, scala.xml, intentionSettings.xml, laf.xml, Immutable Joda Bean.java, vcs.xml, Courier 1_2.icls, BlueForest.icls, databaseSettings.xml, New Method Body.java, Overridden Method Body.java, usageView.xml

---
+++ fileTemplates/code/New Method Body.java
@@ -0,0 +1,2 @@
+// TODO implement ${SIMPLE_CLASS_NAME}.${METHOD_NAME}()
+throw new UnsupportedOperationException("${METHOD_NAME} not implemented");
---
+++ fileTemplates/j2ee/GroovyScript.groovy
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) ${YEAR} - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+#if (${PACKAGE_NAME} && $PACKAGE_NAME != "" )package ${PACKAGE_NAME}
+#end
+
+#parse("File Header.java")
